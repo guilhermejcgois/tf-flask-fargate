04d02a078fd897cf045d6426b3f711a1a20d978f
Guilherme Gois <guijocargo@gmail.com>
2023-09-15 09:26:06 -0300

    style: solves issues with files ident

--- main.tf
+++ main.tf
@@ -1,105 +1,105 @@
 # Provider definition
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 # Variables definition
 variable "exec_task_role" {
-    type = string
+  type = string
 }
 variable "rand_api_image" {
-    type = string
+  type = string
 }
 variable "subnets_id" {
-    type = list
+  type = list(any)
 }
 variable "time_api_image" {
-    type = string
+  type = string
 }
 variable "vpc_id" {
-    type = string
+  type = string
 }
 
 # Security group for the ECS tasks
 resource "aws_security_group" "ecs_sg" {
-    vpc_id = var.vpc_id
-    name = "ecs-security-group"
+  vpc_id = var.vpc_id
+  name   = "ecs-security-group"
 
-    # Inbound and outbound rules
-    ingress {
-        from_port = 5000
-        to_port = 5000
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Inbound and outbound rules
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # ECS task definition
 resource "aws_ecs_task_definition" "task_definition" {
-    family = "tf-flask-fargate-api-task"
-    network_mode = "awsvpc"
-    memory = "1024"
-    requires_compatibilities = ["FARGATE"]
+  family                   = "tf-flask-fargate-api-task"
+  network_mode             = "awsvpc"
+  memory                   = "1024"
+  requires_compatibilities = ["FARGATE"]
 
-    # Task execution role
-    execution_role_arn = var.exec_task_role
+  # Task execution role
+  execution_role_arn = var.exec_task_role
 
-    # Container definitions
-    container_definitions = jsonencode([
+  # Container definitions
+  container_definitions = jsonencode([
+    {
+      name   = "tf-flask-fargate-rand-api-container"
+      image  = var.rand_api_image
+      cpu    = 256
+      memory = 512
+      port_mappings = [
         {
-            name = "tf-flask-fargate-rand-api-container"
-            image = var.rand_api_image 
-            cpu  = 256
-            memory = 512
-            port_mappings = [
-                {
-                    container_port = 5001
-                    host_port = 5000
-                    protocol  = "tcp"
-                }
-            ]
-        },
-        {
-            name = "tf-flask-fargate-time-api-container"
-            image = var.time_api_image 
-            cpu  = 256
-            memory = 512
-            port_mappings = [
-                {
-                    container_port = 5002
-                    host_port = 5000
-                    protocol  = "tcp"
-                }
-            ]
+          container_port = 5001
+          host_port      = 5000
+          protocol       = "tcp"
         }
-    ])
+      ]
+    },
+    {
+      name   = "tf-flask-fargate-time-api-container"
+      image  = var.time_api_image
+      cpu    = 256
+      memory = 512
+      port_mappings = [
+        {
+          container_port = 5002
+          host_port      = 5000
+          protocol       = "tcp"
+        }
+      ]
+    }
+  ])
 
-    # Defining the task-level CPU
-    cpu = "512"
+  # Defining the task-level CPU
+  cpu = "512"
 }
 
 # ECS service
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "tf-flask-fargate-api-cluster"  
+  name = "tf-flask-fargate-api-cluster"
 }
 
 resource "aws_ecs_service" "service" {
-  name = "tf-flask-fargate-api-service"
-  cluster = aws_ecs_cluster.ecs_cluster.id
+  name            = "tf-flask-fargate-api-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count = 1
-  launch_type = "FARGATE"
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
   # Network configuration
   network_configuration {
-    subnets = var.subnets_id
-    security_groups = [aws_security_group.ecs_sg.id]
+    subnets          = var.subnets_id
+    security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
 }
